b05817594e3db97cb6f6c95c054dc14096aecc51
TidbitsJS <tidbitsjs@gmail.com>
2024-12-28 23:43:40 +0530

    reset project and set theme

---
+++ constants/icons.ts
@@ -0,0 +1,99 @@
+import card_icon from "@/assets/icons/card_icon.png";
+import claim_icon from "@/assets/icons/claim_icon.png";
+import dashboard_icon from "@/assets/icons/dashboard_icon.png";
+import menu_icon from "@/assets/icons/menu_icon.png";
+import notification_icon from "@/assets/icons/notification_icon.png";
+import point_icon from "@/assets/icons/point_icon.png";
+import search_icon from "@/assets/icons/search_icon.png";
+import clock_icon from "@/assets/icons/clock_icon.png";
+import page_icon from "@/assets/icons/page_icon.png";
+import page_filled_icon from "@/assets/icons/page_filled_icon.png";
+import plus_icon from "@/assets/icons/plus_icon.png";
+import bookmark_icon from "@/assets/icons/bookmark_icon.png";
+import read_icon from "@/assets/icons/read_icon.png";
+import back_arrow_icon from "@/assets/icons/back_arrow_icon.png";
+import more_icon from "@/assets/icons/more_icon.png";
+import bestseller from "@/assets/icons/bestseller.png";
+import bookstore from "@/assets/icons/bookstore.png";
+import fiction from "@/assets/icons/fiction.png";
+import mic from "@/assets/icons/mic.png";
+import minus from "@/assets/icons/minus.png";
+import plus from "@/assets/icons/plus.png";
+import cart from "@/assets/icons/cart.png";
+import menu from "@/assets/icons/menu.png";
+import bookshelf from "@/assets/icons/bookshelf.png";
+import heart from "@/assets/icons/heart.png";
+import arrowRight from "@/assets/icons/arrow_right.png";
+import read from "@/assets/icons/read.png";
+import help from "@/assets/icons/help.png";
+import language from "@/assets/icons/language.png";
+import privacy from "@/assets/icons/privacy.png";
+import terms from "@/assets/icons/terms.png";
+import notification from "@/assets/icons/notification.png";
+import github from "@/assets/icons/github.png";
+import google from "@/assets/icons/google.png";
+import twitter from "@/assets/icons/twitter.png";
+import apple from "@/assets/icons/apple.png";
+import call from "@/assets/icons/call.png";
+import edit from "@/assets/icons/edit.png";
+import mastercard from "@/assets/icons/mastercard.png";
+import message from "@/assets/icons/message.png";
+import paypal from "@/assets/icons/paypal.png";
+import webmoney from "@/assets/icons/webmoney.png";
+import bhim from "@/assets/icons/bhim.png";
+import bitcoin from "@/assets/icons/bitcoin.png";
+import paytm from "@/assets/icons/paytm.png";
+import creditCard from "@/assets/icons/creditCard.png";
+import cancel from "@/assets/icons/cancel.png";
+import arrowDown from "@/assets/icons/arrowDown.png";
+
+export default {
+  card_icon,
+  claim_icon,
+  dashboard_icon,
+  menu_icon,
+  notification_icon,
+  point_icon,
+  search_icon,
+  clock_icon,
+  page_icon,
+  page_filled_icon,
+  plus_icon,
+  bookmark_icon,
+  read_icon,
+  back_arrow_icon,
+  more_icon,
+  bestseller,
+  bookstore,
+  fiction,
+  mic,
+  minus,
+  plus,
+  cart,
+  menu,
+  bookshelf,
+  heart,
+  read,
+  arrowRight,
+  help,
+  language,
+  privacy,
+  terms,
+  notification,
+  github,
+  google,
+  twitter,
+  apple,
+  call,
+  edit,
+  mastercard,
+  message,
+  paypal,
+  webmoney,
+  bhim,
+  bitcoin,
+  paytm,
+  creditCard,
+  cancel,
+  arrowDown,
+};
---
+++ constants/index.ts
@@ -0,0 +1,4 @@
+import icons from "./icons";
+import theme, { COLORS, SIZES, FONTS, SHADOW } from "./theme";
+
+export { icons, theme, COLORS, SIZES, FONTS, SHADOW };
