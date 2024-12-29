d78121665b5c080f7d7ca2e0eb802feb70cbf42d
TidbitsJS <tidbitsjs@gmail.com>
2024-12-29 16:04:22 +0530

    feat: book description ui

--- app/(core)/books/[id].tsx
+++ app/(core)/books/[id].tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import { COLORS, SIZES } from "@/constants";
+import { COLORS, FONTS, SIZES } from "@/constants";
 import { myBooks } from "@/data/dummy";
 import { getLuminance } from "@/lib/utils";
 import FocusedStatusBar from "@/components/focused-statusbar";
@@ -23,14 +23,42 @@ const BookDetails = () => {
         barStyle={luminance === "#FFFFFF" ? "light-content" : "dark-content"}
       />
 
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: COLORS.black,
-        }}
-      >
-        <BookDetailsHeader book={book as Book} />
-      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: COLORS.black,
+          }}
+        >
+          <BookDetailsHeader book={book as Book} />
+
+          <View
+            style={{
+              padding: SIZES.padding,
+            }}
+          >
+            <Text
+              style={{
+                ...FONTS.h3,
+                color: COLORS.white,
+                marginBottom: SIZES.body2,
+              }}
+            >
+              Description
+            </Text>
+            <View style={{ gap: SIZES.padding }}>
+              {book?.description?.split("\n").map((line, index) => (
+                <Text
+                  key={index}
+                  style={{ ...FONTS.body4, color: COLORS.lightGray }}
+                >
+                  {line}
+                </Text>
+              ))}
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
--- data/books.ts
+++ data/books.ts
@@ -13,7 +13,7 @@ export const bookAtomicHabits = {
   totalCopies: "1.2M",
   availableCopies: "1.1M",
   description:
-    "World-renowned habits expert James Clear has discovered a simpler system for transforming your life. He knows that lasting change comes from the compound effect of hundreds of small decisions – doing two push-ups a day, waking up five minutes early, or holding a single short phone call. He calls them atomic habits. \n In this ground-breaking book, Clear reveals how these tiny changes will help you get 1 percent better every day. He uncovers a handful of simple life hacks (the forgotten art of Habit Stacking, the unexpected power of the Two Minute Rule, or the trick to entering the Goldilocks Zone) and delves into cutting-edge psychology and neuroscience to explain why they matter. Along the way, he tells inspiring stories of Olympic gold medalists, leading CEOs and distinguished scientists who have used the science of small habits to stay productive, motivated and happy.",
+    "World-renowned habits expert James Clear has discovered a simpler system for transforming your life. He knows that lasting change comes from the compound effect of hundreds of small decisions – doing two push-ups a day, waking up five minutes early, or holding a single short phone call. He calls them atomic habits. \n In this ground-breaking book, Clear reveals how these tiny changes will help you get 1 percent better every day. \n He uncovers a handful of simple life hacks (the forgotten art of Habit Stacking, the unexpected power of the Two Minute Rule, or the trick to entering the Goldilocks Zone) and delves into cutting-edge psychology and neuroscience to explain why they matter. \n Along the way, he tells inspiring stories of Olympic gold medalists, leading CEOs and distinguished scientists who have used the science of small habits to stay productive, motivated and happy.",
   backgroundColor: "#ffffff",
 };
 
@@ -30,7 +30,7 @@ export const bookCrackingTheCodingInterview = {
   totalCopies: "1.5M",
   availableCopies: "1.4M",
   description:
-    "Cracking the Coding Interview is the ultimate guide for anyone preparing for a software engineering interview. Gayle Laakmann McDowell, a former software engineer at Google and Microsoft, shares insider tips and strategies to help you succeed. This comprehensive book covers over 150 programming questions with solutions, tips on system design, and advice on how to tackle behavioral questions. It also delves into the best approaches for technical interviews at top tech companies like Google, Amazon, and Microsoft, making it an essential resource for aspiring engineers.",
+    "Cracking the Coding Interview is the ultimate guide for anyone preparing for a software engineering interview. Gayle Laakmann McDowell, a former software engineer at Google and Microsoft, shares insider tips and strategies to help you succeed. \n This comprehensive book covers over 150 programming questions with solutions, tips on system design, and advice on how to tackle behavioral questions. \n It also delves into the best approaches for technical interviews at top tech companies like Google, Amazon, and Microsoft, making it an essential resource for aspiring engineers.",
   backgroundColor: "#97cc02",
 };
 
@@ -47,7 +47,7 @@ export const bookEloquentJavaScript = {
   totalCopies: "1.3M",
   availableCopies: "1.2M",
   description:
-    "Eloquent JavaScript is a deep dive into the JavaScript programming language, designed for readers who already have some basic knowledge of programming. Written by Marijn Haverbeke, this book takes a hands-on approach to learning, teaching not only the syntax of JavaScript but also how to think like a programmer. With interactive examples and exercises, it covers core concepts like functions, higher-order functions, closures, and asynchronous programming, as well as advanced topics like the JavaScript engine and Node.js. This book is ideal for those looking to master JavaScript for web development and beyond.",
+    "Eloquent JavaScript is a deep dive into the JavaScript programming language, designed for readers who already have some basic knowledge of programming. \n Written by Marijn Haverbeke, this book takes a hands-on approach to learning, teaching not only the syntax of JavaScript but also how to think like a programmer. \n With interactive examples and exercises, it covers core concepts like functions, higher-order functions, closures, and asynchronous programming, as well as advanced topics like the JavaScript engine and Node.js. \n This book is ideal for those looking to master JavaScript for web development and beyond.",
   backgroundColor: "#F8D347",
 };
 
@@ -64,6 +64,6 @@ export const bookReactInAction = {
   totalCopies: "1.1M",
   availableCopies: "1M",
   description:
-    "React in Action is a comprehensive guide for learning React, one of the most popular JavaScript libraries for building user interfaces. Written by Mark T. Thomas, this book covers both the fundamentals and advanced techniques of React development. It provides hands-on examples of building modern, dynamic applications using React’s component-based architecture. The book walks through creating stateful components, integrating APIs, managing application state with Redux, and deploying React applications. Whether you’re a beginner or an intermediate React developer, this book will help you take your skills to the next level.",
+    "React in Action is a comprehensive guide for learning React, one of the most popular JavaScript libraries for building user interfaces. Written by Mark T. \n Thomas, this book covers both the fundamentals and advanced techniques of React development. It provides hands-on examples of building modern, dynamic applications using React’s component-based architecture. \n The book walks through creating stateful components, integrating APIs, managing application state with Redux, and deploying React applications. \n Whether you’re a beginner or an intermediate React developer, this book will help you take your skills to the next level.",
   backgroundColor: "#302428",
 };
