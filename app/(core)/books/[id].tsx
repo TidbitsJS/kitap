a5c2292cbad349ea8c2d39e8e3664eb4cd7d6873
TidbitsJS <tidbitsjs@gmail.com>
2024-12-29 17:04:54 +0530

    feat: details action button

--- app/(core)/books/[id].tsx
+++ app/(core)/books/[id].tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { View, Text, ScrollView } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
 
-import { COLORS, FONTS, SIZES } from "@/constants";
 import { myBooks } from "@/data/dummy";
 import { getLuminance } from "@/lib/utils";
+import { COLORS, FONTS, icons, SIZES } from "@/constants";
+
 import FocusedStatusBar from "@/components/focused-statusbar";
 import BookDetailsHeader from "@/components/book/details/header";
 
@@ -23,7 +24,13 @@ const BookDetails = () => {
         barStyle={luminance === "#FFFFFF" ? "light-content" : "dark-content"}
       />
 
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          backgroundColor: COLORS.black,
+          paddingBottom: 100,
+        }}
+      >
         <View
           style={{
             flex: 1,
@@ -59,6 +66,60 @@ const BookDetails = () => {
           </View>
         </View>
       </ScrollView>
+
+      <View
+        style={{
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          backgroundColor: COLORS.black,
+          padding: SIZES.padding / 1.25,
+          flexDirection: "row",
+          justifyContent: "space-between",
+          borderTopWidth: 1,
+          borderColor: COLORS.secondary,
+          gap: SIZES.body1,
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            width: 50,
+            backgroundColor: COLORS.secondary,
+            borderRadius: 25,
+            padding: SIZES.font,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          onPress={() => console.log("Bookmark")}
+        >
+          <Image
+            source={icons.bookmark_icon}
+            resizeMode="contain"
+            style={{
+              width: 18,
+              height: 18,
+              tintColor: COLORS.lightGray2,
+            }}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{
+            flex: 1,
+            backgroundColor: COLORS.primary,
+            borderRadius: SIZES.radius,
+            paddingHorizontal: SIZES.font,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          onPress={() => console.log("Start Reading")}
+        >
+          <Text style={{ ...FONTS.h3, color: COLORS.white }}>
+            Start Reading
+          </Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
